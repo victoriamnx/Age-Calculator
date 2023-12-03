b1de324f1b7e605d0b7a901bec7827ebe1a4c3be
Victória Nogueira Marques <victoriamarques0604@gmail.com>
2023-12-02 22:49:16 -0300

    Update

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,30 @@
+import { Input } from "./components/ui/input";
+import { Label } from "@radix-ui/react-label";
+
 function App() {
   return (
     <main className="flex justify-center items-center h-screen font-poppins bg-gray-100">
-      <div className="flex flex-col bg-white p-14 rounded-tl-lg rounded-r-lg gap-8">
-        <div className="flex  ">
+      <div className="flex flex-col bg-white p-14 rounded-3xl gap-8">
+        <div className="flex">
           <div className="flex flex-col">
             <label className="text-gray-400 font-poppins font-bold" htmlFor="">
               DAY
             </label>
-            <input type="text" />
+            <Input />
           </div>
 
           <div className="flex flex-col">
             <label className="text-gray-400 font-poppins font-bold" htmlFor="">
               MONTH
             </label>
-            <input type="text" />
+            <Input />
           </div>
 
           <div className="flex flex-col">
             <label className="text-gray-400 font-poppins font-bold" htmlFor="">
               YEAR
             </label>
-            <input type="text" />
+            <Input />
           </div>
         </div>
 
@@ -29,16 +32,22 @@ function App() {
 
         <div className="flex flex-col">
           <div className="flex gap-2">
-            <h1 className="text-purple-500  italic font-bold text-8xl">0</h1>
-            <h2 className="text-8xl italic font-bold">days</h2>
+            <h1 className="text-purple-500 font-poppins italic font-bold text-8xl">
+              0
+            </h1>
+            <h2 className="text-8xl font-poppins italic font-bold">days</h2>
           </div>
           <div className="flex gap-2">
-            <h1 className="text-purple-500 italic font-bold text-8xl">0</h1>
-            <h2 className="text-8xl  italic font-bold">months</h2>
+            <h1 className="text-purple-500 font-poppins italic font-bold text-8xl">
+              0
+            </h1>
+            <h2 className="text-8xl font-poppins italic font-bold">months</h2>
           </div>
           <div className="flex gap-2">
-            <h1 className="text-purple-500 italic font-bold text-8xl">0</h1>
-            <h2 className="text-8xl italic font-bold">years</h2>
+            <h1 className="text-purple-500 font-poppins italic font-bold text-8xl">
+              0
+            </h1>
+            <h2 className="text-8xl italic font-poppins font-bold">years</h2>
           </div>
         </div>
       </div>
