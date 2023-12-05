fbd96acb0c9be7e206e6ccf141e65095579f90a3
Victória Nogueira Marques <victoriamarques0604@gmail.com>
2023-12-04 21:30:04 -0300

    fix: input and placeholder

--- src/app.tsx
+++ src/app.tsx
@@ -2,28 +2,46 @@ import { Input } from "./components/ui/input";
 
 function App() {
   return (
-    <main className="flex justify-center items-center h-screen font-poppins bg-gray-100">
+    <main className="flex justify-center items-center h-screen font-poppins bg-[#F0F0F0]">
       <div className="flex flex-col bg-white p-14 rounded-3xl gap-8">
-        <div className="flex">
+        <div className="flex  gap-8">
           <div className="flex flex-col">
-            <label className="text-gray-400 font-poppins font-bold" htmlFor="">
+            <label
+              className="text-[#716F6F] tracking-[0.21875rem] font-poppins text-[0.875rem] font-bold"
+              htmlFor=""
+            >
               DAY
             </label>
-            <Input />
+            <Input
+              className="w-[10rem] h-[4.5rem] placeholder:text-[rgba(21, 21, 21, 0.5)] placeholder:text-[2rem] font-bold"
+              placeholder="DD"
+            />
           </div>
 
           <div className="flex flex-col">
-            <label className="text-gray-400 font-poppins font-bold" htmlFor="">
+            <label
+              className="text-[#716F6F] tracking-[0.21875rem] font-poppins text-[0.875rem] font-bold"
+              htmlFor=""
+            >
               MONTH
             </label>
-            <Input />
+            <Input
+              className=" w-[10rem] h-[4.5rem] placeholder:text-[rgba(21, 21, 21, 0.5)] placeholder:text-[2rem] font-bold"
+              placeholder="MM"
+            />
           </div>
 
           <div className="flex flex-col">
-            <label className="text-gray-400 font-poppins font-bold" htmlFor="">
+            <label
+              className="text-[#716F6F] tracking-[0.21875rem] font-poppins text-[0.875rem] font-bold"
+              htmlFor=""
+            >
               YEAR
             </label>
-            <Input />
+            <Input
+              className="w-[10rem] h-[4.5rem] placeholder:text-[rgba(21, 21, 21, 0.5)] placeholder:text-[2rem] font-bold"
+              placeholder="YYYY"
+            />
           </div>
         </div>
 
