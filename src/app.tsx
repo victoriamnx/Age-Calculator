742aba9af99d41066c70fdcff75ca2c627fd71bd
Victória Nogueira Marques <victoriamarques0604@gmail.com>
2023-12-05 22:43:09 -0300

    add: functions

--- src/app.tsx
+++ src/app.tsx
@@ -1,41 +1,148 @@
+import { useState, useEffect } from "react";
 import { Input } from "./components/ui/input";
-import { useState } from "react";
+import {
+  differenceInYears,
+  differenceInMonths,
+  differenceInDays,
+  parse,
+} from "date-fns";
 
 function App() {
   const [hovered, setHovered] = useState(false);
+  const [dayValue, setDayValue] = useState("");
+  const [monthValue, setMonthValue] = useState("");
+  const [yearValue, setYearValue] = useState("");
+  const [dayError, setDayError] = useState("");
+  const [monthError, setMonthError] = useState("");
+  const [yearError, setYearError] = useState("");
+  const [yearsResult, setYearsResult] = useState("--");
+  const [monthsResult, setMonthsResult] = useState("--");
+  const [daysResult, setDaysResult] = useState("--");
+
+  useEffect(() => {
+    calculateResults();
+  }, [dayValue, monthValue, yearValue]);
+
+  const validateDay = () => {
+    const dayNumber = parseInt(dayValue, 10);
+    if (isNaN(dayNumber) || dayNumber < 1 || dayNumber > 31) {
+      setDayError("Must be a valid day");
+    } else {
+      setDayError("");
+    }
+  };
+
+  const validateMonth = () => {
+    const monthNumber = parseInt(monthValue, 10);
+    if (isNaN(monthNumber) || monthNumber < 1 || monthNumber > 12) {
+      setMonthError("Must be a valid month");
+    } else {
+      setMonthError("");
+    }
+  };
+
+  const validateYear = () => {
+    const yearNumber = parseInt(yearValue, 10);
+    if (isNaN(yearNumber) || yearNumber.toString().length > 4) {
+      setYearError("Must be a valid year");
+    } else {
+      setYearError("");
+    }
+  };
+
+  const calculateResults = () => {
+    const dayNumber = parseInt(dayValue, 10);
+    const monthNumber = parseInt(monthValue, 10);
+    const yearNumber = parseInt(yearValue, 10);
+
+    if (!isNaN(dayNumber) && !isNaN(monthNumber) && !isNaN(yearNumber)) {
+      const inputDate = parse(
+        `${yearNumber}-${monthNumber.toString().padStart(2, "0")}-${dayNumber
+          .toString()
+          .padStart(2, "0")}`,
+        "yyyy-MM-dd",
+        new Date()
+      );
+      const currentDate = new Date();
+
+      const yearsDiff = differenceInYears(currentDate, inputDate);
+      const monthsDiff = differenceInMonths(currentDate, inputDate) % 12; // Calcular o resto para obter o número de meses dentro do ano
+      const daysDiff = differenceInDays(currentDate, inputDate);
+
+      setYearsResult(`${yearsDiff}`);
+      setMonthsResult(`${monthsDiff}`);
+      setDaysResult(`${daysDiff}`);
+    } else {
+      setYearsResult("--");
+      setMonthsResult("--");
+      setDaysResult("--");
+    }
+  };
 
   return (
     <main className="flex justify-center items-center h-screen font-poppins bg-[#F0F0F0]">
       <div className="flex flex-col bg-white p-14 rounded-t-[1.5rem] rounded-bl-[1.5rem] rounded-br-[12.5rem]">
         <div className="flex gap-8">
-          <div className="flex flex-col gap-[0.5rem]">
+          <div
+            className={`flex flex-col gap-[0.5rem] ${dayError ? "error" : ""}`}
+          >
             <label
-              className="text-[#716F6F] tracking-[0.21875rem] font-poppins text-[0.875rem] font-bold"
+              className={`text-[#716F6F] tracking-[0.21875rem] font-poppins text-[0.875rem] font-bold ${
+                dayError ? "error" : ""
+              }`}
               htmlFor=""
             >
               DAY
             </label>
-            <Input placeholder="DD" />
+            <Input
+              placeholder="DD"
+              value={dayValue}
+              onChange={(e) => setDayValue(e.target.value)}
+              onBlur={validateDay}
+            />
+            {dayError && <span className="error-message">{dayError}</span>}
           </div>
 
-          <div className="flex flex-col gap-[0.5rem]">
+          <div
+            className={`flex flex-col gap-[0.5rem] ${
+              monthError ? "error" : ""
+            }`}
+          >
             <label
-              className="text-[#716F6F] tracking-[0.21875rem] font-poppins text-[0.875rem] font-bold"
+              className={`text-[#716F6F] tracking-[0.21875rem] font-poppins text-[0.875rem] font-bold ${
+                monthError ? "error" : ""
+              }`}
               htmlFor=""
             >
               MONTH
             </label>
-            <Input placeholder="MM" />
+            <Input
+              placeholder="MM"
+              value={monthValue}
+              onChange={(e) => setMonthValue(e.target.value)}
+              onBlur={validateMonth}
+            />
+            {monthError && <span className="error-message">{monthError}</span>}
           </div>
 
-          <div className="flex flex-col gap-[0.5rem]">
+          <div
+            className={`flex flex-col gap-[0.5rem] ${yearError ? "error" : ""}`}
+          >
             <label
-              className="text-[#716F6F] tracking-[0.21875rem] font-poppins text-[0.875rem] font-bold"
+              className={`text-[#716F6F] tracking-[0.21875rem] font-poppins text-[0.875rem] font-bold ${
+                yearError ? "error" : ""
+              }`}
               htmlFor=""
             >
               YEAR
             </label>
-            <Input placeholder="YYYY" />
+            <Input
+              placeholder="YYYY"
+              value={yearValue}
+              onChange={(e) => setYearValue(e.target.value)}
+              onBlur={validateYear}
+            />
+            {yearError && <span className="error-message">{yearError}</span>}
           </div>
         </div>
 
@@ -79,19 +186,19 @@ function App() {
         <div className="flex flex-col">
           <div className="flex gap-2">
             <h1 className="text-[#854DFF] font-poppins italic font-bold text-8xl">
-              --
+              {yearsResult}
             </h1>
             <h2 className="text-8xl italic font-poppins font-[800]">years</h2>
           </div>
           <div className="flex gap-2">
             <h1 className="text-[#854DFF] font-poppins italic font-bold text-8xl">
-              --
+              {monthsResult}
             </h1>
             <h2 className="text-8xl font-poppins italic font-[800]">months</h2>
           </div>
           <div className="flex gap-2">
             <h1 className="text-[#854DFF] font-poppins italic font-bold text-8xl">
-              --{" "}
+              {daysResult}
             </h1>
             <h2 className="text-8xl font-poppins italic font-[800]">days</h2>
           </div>
