566031fdf3485d642cd8cd09ab68becebe225e92
Victória Nogueira Marques <victoriamarques0604@gmail.com>
2023-12-06 21:04:41 -0300

    fix: mobile input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex w-[10rem] px-[1.5rem] py-[0.75rem] rounded-md border border-input bg-transparent text-[2rem] focus:outline-none focus:ring focus:border-[#854DFF] placeholder:text-[rgba(21, 21, 21, 0.5)] placeholder:text-[2rem] font-bold cursor-pointer shadow-sm transition-colors file:border-0 file:bg-transparent focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed ",
+          "flex max-[700px]:w-[5.5rem] w-[10rem] max-[700px]:px-[1rem] py-[0.75rem] px-[1.5rem] rounded-md border border-input bg-transparent max-[700px]:text-[1.25rem] text-[2rem] focus:outline-none focus:ring focus:border-[#854DFF] placeholder:text-[rgba(21, 21, 21, 0.5)] max-[700px]:placeholder:text-[1.25rem] placeholder:text-[2rem] font-bold cursor-pointer shadow-sm transition-colors file:border-0 file:bg-transparent focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed ",
           className
         )}
         ref={ref}
