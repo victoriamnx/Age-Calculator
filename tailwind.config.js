111c28406ea71bea0990bdc93cb84997dc989154
Victória Nogueira Marques <96449803+victoriamnx@users.noreply.github.com>
2023-12-12 17:37:07 -0300

    add: font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,5 +69,8 @@ export const theme = {
       "accordion-down": "accordion-down 0.2s ease-out",
       "accordion-up": "accordion-up 0.2s ease-out",
     },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
   },
 };
